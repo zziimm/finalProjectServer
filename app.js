df186dfc7ed24449a788ab791ce4608c1e61d5c8
zziimm <wlalscjs30@naver.com>
2023-12-20 14:01:10 +0900

    서버 셋팅

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const path = require('path');
+const cors = require('cors');
+const paasport = require('passport');
+
+dotenv.config();
+
+const app = express();
+app.set('port', process.env.PORT || 8088);
+// passportConfig();
+// connect();
+
+app.use(cors({
+  origin: 'https://minton1000.netlify.app',
+  credentials: true
+}));
+app.use(morgan('dev'));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(express.json())
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+  name: 'session-cookie'
+}));
+app.use(paasport.initialize());
+app.use(paasport.session());
+
+
+
+
+
+
+
+
+
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err)
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port') + '번에서 서버 실행 중입니다.');
+});
