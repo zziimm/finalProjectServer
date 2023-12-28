425a21f6abce062ac0525c48368cac41e41b83bf
off0405 <off0405@naver.com>
2023-12-28 17:51:34 +0900

    12.28 중고 마켓 구축 완료 - ejs로 이것저것 실험중 - userInfo가져오기

--- routes/vintage.js
+++ routes/vintage.js
@@ -33,34 +33,36 @@ const upload = multer({
   limits: { fieldSize: 5 * 1024 * 1024 }
 });
 
+// router.get('/upload', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'multipart.html'));
+// });
+
 
 
 // 중고커뮤
 router.get('/', async (req, res) => {
-  try {
-    const data = await db.collection('vincommunity').find({ type: 'vintage' }).toArray()
-    res.json({
-      flag: true,
-      message: '데이터 불러오기 성공(중고)',
-      data
-    })
-  } catch (err) {
-    console.error(err);
-  }
-})
+  const posts = await db.collection('vincommunity').find().skip((req.query.page - 1) * 5).limit(5).toArray();
+  const totalCount = await db.collection('vincommunity').countDocuments({}); // 전체 document 개수
+  const postsPerPage = 5; // 페이지 당 콘텐츠 개수
+  const numOfPage = Math.ceil(totalCount / postsPerPage); // 페이지 수
+  const currentPage = req.query.page || 1; // 현재 페이지
+
+  res.render('vintage', { posts, numOfPage, currentPage });
+});
 
 router.get('/detail/:postId', async (req, res) => {
   const postId = req.params.postId
-  const postData = await db.colleciont('vincommunity').findOne({ _id: new ObjectId(postId)})
-  const userData = await db.collection('userInfo').findOne({ _id: postData._id })
+  const posts = await db.collection('vincommunity').findOne({ _id: new ObjectId(postId)})
+  const userData = await db.collection('userInfo').findOne({ _id: posts._id })
+  console.log(userData);
   // 조회수
   const views = await db.collection('vincommunity').updateOne({ _id: new ObjectId(postId) }, { $inc: { views: 1 }  })
-    res.render('vintage', { postData, userData, views });
+    res.render('vintage', { posts, userData, views });
 
   res.json({
     flag: true,
     message: '데이터 불러오기 성공(상세보기)',
-    postData,
+    posts,
     userData,
     views
   })
@@ -72,18 +74,17 @@ router.get('/insert', (req, res) => {
 
 // 커뮤니티 삽입_중고
 router.post('/insert', upload.array('img'), async (req, res) => {
-  const { title, content, price, category, date } = req.body;
-  const username = req.user.username
-  const dog = req.user.dogSpecies
+  const { title, content, price, category } = req.body;
+  // const user = req.user.userId
+  // console.log(user);
+  // const dog = req.user.dogSpecies
   const imgUrl = req.files?.location || ''
   const imgKey = req.files?.key || ''
-  console.log('-------------------');
-    console.log(username);
 
   
   try {
     await db.collection('vincommunity').insertOne({
-    username, dog, title, content, price, category, date, imgUrl, imgKey, type: 'vintage'
+    title, content, price, category, imgUrl, imgKey
     })
     res.json({
       flag: true,
